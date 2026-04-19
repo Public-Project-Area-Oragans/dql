309fbc082d8dc589aa85de5067a141ca095a0881
VelkaressiaBlutkrone <VelkaressiaBlutkrone@users.noreply.github.com>
2026-04-19 03:14:44 +0000

    deploy: 458b07c39be435725e397ee9371760acf6dadc65

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2577025333" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "627817544" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
